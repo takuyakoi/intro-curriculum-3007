0634472132a07354ddc98655fb57fc6b81f2ffe5
takuyakoi <takuya.koiso.s@gmail.com>
2020-07-25 07:31:35 +0900

    isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数の見返す
+ * @param {array} input 
+ */
+function isMultipleOfSeventeen(input){
+    return input%17===0;
+}
+
+module.exports = { isMultipleOfSeventeen
 }
